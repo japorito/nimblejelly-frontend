73bf5e19abae0e2d9fedd9586d4296943d3d0126
Jacob Saporito <japorito@gmail.com>
2022-04-29 20:00:11 -0700

    Add material ui, and start to put in theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { ThemeProvider, createTheme, ThemeOptions } from '@mui/material/styles';
+import { AppBar } from '@mui/material';
+
 
 function App() {
+  const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)');
+
+  const themeOpts: ThemeOptions =
+  prefersLightMode
+  ? {
+    palette: {
+      primary: {
+        main: '#800080',
+      },
+      secondary: {
+        main: '#068029',
+      },
+      success: {
+        main: '#068029',
+      },
+      error: {
+        main: '#800000',
+      },
+      warning: {
+        main: '#ccca14',
+      },
+      info: {
+        main: '#2196f3',
+      },
+    },
+  }
+  : {
+    palette: {
+      primary: {
+        main: '#14cc48',
+      },
+      secondary: {
+        main: '#068029',
+      },
+      background: {
+        default: '#710078',
+        paper: '#55006b',
+      },
+      text: {
+        primary: '#ffffff',
+        secondary: '#ffffff',
+        disabled: 'rgba(255,255,255,0.6)',
+        //hint: '#ffffff',
+      },
+      error: {
+        main: '#800606',
+        contrastText: '#800606',
+      },
+      success: {
+        main: '#068029',
+      },
+    },
+    components :
+    {
+      MuiAppBar: {
+        styleOverrides: {
+          colorInherit: {
+            backgroundColor: '#55006b',
+            color: '#ffffff',
+          },
+        },
+        defaultProps: {
+          color: 'inherit'
+        }
+      }
+    },
+  };
+
+  const theme = createTheme(themeOpts);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppBar>This is my app bar</AppBar>
+    </ThemeProvider>
   );
 }
 
